2db3e62f6bbd1fafcee7bcc1db3d6b795a2bae4b
newkayak12 <newkayak12@gmail.com>
2021-03-27 15:52:34 +0900

    baekjoon

---
+++ baekjoon/src/ifStatement/Pnum1330_compare.java
@@ -0,0 +1,25 @@
+package ifStatement;
+
+public class Pnum1330_compare {
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+			System.out.println(a+"\n");
+			System.out.println(b);
+			
+		if(a>b) {
+			System.out.println(">");
+			
+		} else if (a<b) {
+			
+			System.out.println("<");
+			
+		} else {
+			
+			System.out.println("==");
+		}
+		
+	}
+		
+	
+}

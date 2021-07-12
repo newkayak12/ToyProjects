5b97030558ec36f160953ee867d47fb6df67bf9f
newkayak12 <newkayak12@gmail.com>
2021-07-12 22:39:08 +0900

    a+b

---
+++ baekjoon/src/basicMath/Pnum_10757_aPlusb.java
@@ -0,0 +1,20 @@
+package basicMath;
+
+import java.util.Scanner;
+
+public class Pnum_10757_aPlusb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		
+				System.out.println("A");
+			long aNumber = scn.nextLong();
+				System.out.println("B");
+			long bNumber = scn.nextLong();
+			
+				System.out.println("A+B = "+(aNumber+bNumber));
+			
+	}
+
+}
